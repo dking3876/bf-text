e6ee42525fb2972a6ee0ca4d78a217cabb396664
dking3876 <dking3876@msn.com>
2016-12-20 21:15:35 -0500

    finished project folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,35 +10,29 @@ const inlineResources = require('./scripts/inline');
 
 //first build the project and copy to dist folder
 gulp.task(':build-dist', ()=>{
-    //.pipe(gulp.dest('build'));
-
     return gulp
     .src('src/**/*', {base: './src'})
     //.pipe(gulpCopy('dist/'));
     .pipe(gulp.dest('build'));
 })
-gulp.task(':publish',[':inline-resources'], ()=>{
+gulp.task(':npm-version',[':inline-resources'], ()=>{
     return run('npm version patch').exec();
     //eval('npm publish dist/src');
 })
-
-gulp.task(":compile", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
-});
-
-//now inline all the resources
-gulp.task(':inline-resources',[':build-dist'], () => inlineResources(DIST_COMPONENTS_ROOT));
-
-gulp.task('default', [':inline-resources']);
-
-gulp.task('build', [':build-dist']);
-
-gulp.task('publish', [':build-dist', ':inline-resources', ':publish'], ()=>{
+gulp.task(':cp-package', ()=>{
     gulp
     .src('package.json')
     .pipe(gulpCopy('build/'));
+})
+//now inline all the resources
+gulp.task(':inline-resources',[':build-dist'], () => inlineResources(DIST_COMPONENTS_ROOT));
+
+gulp.task('default', ['build']);
+
+gulp.task('build', [':build-dist', ':inline-resources']);
+
+gulp.task('publish', [':build-dist', ':inline-resources', ':npm-version', ':cp-package'], ()=>{
+    return run('npm publish build')
 })
 
 gulp.task('typescript-compile', [':compile']);
